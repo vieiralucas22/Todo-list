85d5d637a81997ddf8380a282e5898582910e074
Lucas Vieira Brito <vieiralucas2205@gmail.com>
2022-10-18 11:01:25 -0300

    Terminando tasks dinaminas

--- JS/script.js
+++ JS/script.js
@@ -6,31 +6,43 @@ btnCreateTask.addEventListener("click", function () {
   let inputTask = document.getElementById("inputTask");
 
   const taskLine = document.createElement("div");
+  const checkboxTask = document.createElement("input");
   const task = document.createElement("p");
   const btnArea = document.createElement("div");
   const btnEdit = document.createElement("button");
   const iconEdit = document.createElement("i");
   const btnDelete = document.createElement("button");
   const iconDelete = document.createElement("i");
+  const divCheckTask = document.createElement("div");
 
-  taskLine.classList.add("task");
-  taskLine.id = `task-${contTask}`;
-  task.innerText = `${inputTask.value}`;
-  btnArea.classList.add("btnArea");
-  btnEdit.classList.add("btn", "btn-outline-warning");
-  btnEdit.id = `editTask-${contTask}`;
-  iconEdit.classList.add("bi", "bi-pencil");
-  btnDelete.classList.add("btn", "btn-outline-danger");
-  btnDelete.id = `deleteTask-${contTask}`;
-  iconDelete.classList.add("bi", "bi-trash");
+  if (inputTask.value) {
+    taskLine.classList.add("task");
+    taskLine.id = `task-${contTask}`;
+    divCheckTask.classList.add("taskCheck");
+    task.innerText = `${inputTask.value}`;
+    task.id = `task-${contTask}`;
+    checkboxTask.type = "checkbox";
+    checkboxTask.id = `check-${contTask}`;
+    btnArea.classList.add("btnArea");
+    btnEdit.classList.add("btn", "btn-outline-warning");
+    btnEdit.id = `editTask-${contTask}`;
+    iconEdit.classList.add("bi", "bi-pencil");
+    btnDelete.classList.add("btn", "btn-outline-danger");
+    btnDelete.id = `deleteTask-${contTask}`;
+    iconDelete.classList.add("bi", "bi-trash");
 
-  btnEdit.appendChild(iconEdit);
-  btnDelete.appendChild(iconDelete);
-  btnArea.appendChild(btnEdit);
-  btnArea.appendChild(btnDelete);
-  taskLine.append(task, btnArea);
-  areaTasks.appendChild(taskLine);
+    divCheckTask.appendChild(checkboxTask);
+    divCheckTask.appendChild(task);
+    btnEdit.appendChild(iconEdit);
+    btnDelete.appendChild(iconDelete);
+    btnArea.appendChild(btnEdit);
+    btnArea.appendChild(btnDelete);
+    taskLine.append(divCheckTask, btnArea);
+    areaTasks.appendChild(taskLine);
 
-  contTask++;
-  inputTask.value = "";
+    contTask++;
+    inputTask.value = "";
+  } else {
+    alert("Digite uma task!!!");
+  }
 });
